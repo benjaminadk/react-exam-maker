74590f52c1a028ca1502903efe8bdd108bda7adb
benjaminadk <benjaminadk@gmail.com>
2018-10-27 02:31:41 -0700

    fix json feature

--- routes/getExam.js
+++ routes/getExam.js
@@ -1,6 +1,20 @@
 const models = require('../models')
 
+function removeTypename(obj) {
+  Object.keys(obj).forEach(key1 => {
+    key1 === '_id' && delete obj[key1]
+    key1 === '__v' && delete obj[key1]
+    Array.isArray(obj[key1]) &&
+      obj[key1].forEach(el => {
+        typeof el === 'object' && removeTypename(el)
+      })
+  })
+  return obj
+}
+
 module.exports = async (req, res) => {
   let exam = await models.Exam.findById(req.query.examId)
-  res.json(exam)
+  let str = JSON.stringify(exam)
+  let payload = removeTypename(JSON.parse(str))
+  res.send(JSON.stringify(payload))
 }
